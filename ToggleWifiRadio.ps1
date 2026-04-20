06e052ae890b1334d59c07f5650ecb86efacf9bc
Golden <goldenlandscape731@gmail.com>
2026-04-19 21:35:49 -0700

    Update ToggleWifiRadio with a restart command

--- ToggleWifiRadio.ps1
+++ ToggleWifiRadio.ps1
@@ -4,8 +4,12 @@
 # Turn on
 # PS D:\path\to\your\folder> .\ToggleWifiRadio.ps1 -Status 'On'
 
-[CmdletBinding()] Param (
-    [Parameter(Mandatory=$true)][ValidateSet('Off', 'On')][string]$Status
+# Off, wait 5s, then on
+# PS D:\path\to\your\folder> .\ToggleWifiRadio.ps1 -Restart
+
+[CmdletBinding(DefaultParameterSetName='Status')] Param (
+    [Parameter(ParameterSetName='Status', Mandatory=$true)][ValidateSet('Off', 'On')][string]$Status,
+    [Parameter(ParameterSetName='Restart', Mandatory=$true)][switch]$Restart
 )
 
 Add-Type -AssemblyName System.Runtime.WindowsRuntime
@@ -22,4 +26,10 @@ Await ([Windows.Devices.Radios.Radio]::RequestAccessAsync()) ([Windows.Devices.R
 $radios = Await ([Windows.Devices.Radios.Radio]::GetRadiosAsync()) ([System.Collections.Generic.IReadOnlyList[Windows.Devices.Radios.Radio]])
 $wifiRadio = $radios | ? { $_.Kind -eq 'WiFi' }
 [Windows.Devices.Radios.RadioState,Windows.System.Devices,ContentType=WindowsRuntime] | Out-Null
-Await ($wifiRadio.SetStateAsync($Status)) ([Windows.Devices.Radios.RadioAccessStatus]) | Out-Null
+if ($Restart) {
+    Await ($wifiRadio.SetStateAsync('Off')) ([Windows.Devices.Radios.RadioAccessStatus]) | Out-Null
+    Start-Sleep -Seconds 5
+    Await ($wifiRadio.SetStateAsync('On')) ([Windows.Devices.Radios.RadioAccessStatus]) | Out-Null
+} else {
+    Await ($wifiRadio.SetStateAsync($Status)) ([Windows.Devices.Radios.RadioAccessStatus]) | Out-Null
+}
